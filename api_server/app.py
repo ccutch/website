be101e6e9a005f755d2e68a401bfd9f96c1a26ba
Connor McCutcheon <connor@table.co>
2017-04-15 18:25:23 -0700

    fixing relative route

--- api_server/app.py
+++ api_server/app.py
@@ -6,5 +6,5 @@ from flask_restplus import Api
 api_routes = Blueprint(__name__, 'api_routes')
 api = Api(api_routes, title='API for ccutch site', validate=True, doc='/api/')
 
-import notebook_routes  # noqa
-import entry_routes  # noqa
+import .notebook_routes  # noqa
+import .entry_routes  # noqa
